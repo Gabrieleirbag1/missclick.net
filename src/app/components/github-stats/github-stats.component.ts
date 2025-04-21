f4cf0184e7bef1612fb00b345a9c39c84f0432c4
Gabriel <gabrielgarrone670@gmail.com>
2025-04-21 20:25:41 +0200

    feat: added winxp desktop for github stats

--- src/app/components/github-stats/github-stats.component.ts
+++ src/app/components/github-stats/github-stats.component.ts
@@ -1,29 +1,71 @@
-import { Component, OnInit } from '@angular/core';
-import { GithubService } from '../../services/github.service';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { GithubService } from '../../services/github.service';
 
 @Component({
   selector: 'app-github-stats',
-  templateUrl: './github-stats.component.html',
-  styleUrls: ['./github-stats.component.css'],
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule],
+  templateUrl: './github-stats.component.html',
+  styleUrls: ['./github-stats.component.css']
 })
 export class GithubStatsComponent implements OnInit {
-  githubData: any;
-  repos: any[] = [];
-
-  constructor(private githubService: GithubService) {}
-
+  username = 'Gabrieleirbag1';
+  activeModal: string | null = null;
+  currentTime = new Date();
+  githubStats: any = {};
+  
+  constructor(
+    private githubService: GithubService,
+    private ngZone: NgZone
+  ) {}
+  
   ngOnInit(): void {
-    // this.githubService.getUserProfile().subscribe((data) => {
-    //   this.githubData = data;
-    // });
+    // Update the time every minute
+    this.githubStats.stars = 12;
 
-    // this.githubService.getRepos().subscribe((data) => {
-    //   this.repos = data;
-    // });
-
+    setInterval(() => {
+      this.ngZone.run(() => {
+        this.currentTime = new Date();
+      });
+    }, 60000);
+  }
+  
+  openModal(modalType: string): void {
+    this.activeModal = modalType;
+  }
+  
+  closeModal(): void {
+    this.activeModal = null;
+  }
+  
+  getModalTitle(): string {
+    switch(this.activeModal) {
+      case 'languages':
+        return 'Programming Languages';
+      case 'profile':
+        return 'GitHub Profile Stats';
+      case 'contributions':
+        return 'Contribution Streak';
+      case 'stars':
+        return 'Starred Repositories';
+      default:
+        return 'GitHub Statistics';
+    }
+  }
+  
+  getModalIcon(): string {
+    switch(this.activeModal) {
+      case 'languages':
+        return 'assets/icons/chart-icon.png';
+      case 'profile':
+        return 'assets/icons/github-icon.png';
+      case 'contributions':
+        return 'assets/icons/calendar-icon.png';
+      case 'stars':
+        return 'assets/icons/star-icon.png';
+      default:
+        return 'assets/icons/github-icon.png';
+    }
   }
 }
