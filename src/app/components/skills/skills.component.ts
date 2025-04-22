d93324063c0b6455bf5212b431e6157efdaabc7f
Gabriel <gabrielgarrone670@gmail.com>
2025-04-22 16:10:58 +0200

    feat: add expandable cards for skills presentation

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -7,6 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
+
+
 export class SkillsComponent {
 
+  expandCard(event: Event) {
+    const clickedCard = (event.currentTarget as HTMLElement);
+    const allCards = document.querySelectorAll('.skill-card');
+    
+    const isExpanded = clickedCard.classList.contains('expanded');
+    
+    allCards.forEach(card => {
+      card.classList.remove('expanded');
+      card.classList.remove('reduced');
+    });
+    
+    if (!isExpanded) {
+      clickedCard.classList.add('expanded');
+      
+      allCards.forEach(card => {
+        if (card !== clickedCard) {
+          card.classList.add('reduced');
+        }
+      });
+    }
+  }
 }
