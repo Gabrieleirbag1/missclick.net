453d46c26cf68f46a2a6e32d7d1cc60f56a5c059
Gabriel <gabrielgarrone670@gmail.com>
2025-04-25 11:32:01 +0200

    feat: add project component and piano.png

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },  // Default route to home
   { path: 'home', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent},
   { path: '**', redirectTo: '' }  // Redirect any unknown routes to home
 ];
