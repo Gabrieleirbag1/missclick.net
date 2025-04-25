62338f917791f63fa17a221e82830f35861a3f71
Gabriel <gabrielgarrone670@gmail.com>
2025-04-25 15:23:29 +0200

    feat: Implement hobbies slider with dynamic content and improved styles

--- src/app/components/hobbies/hobbies.component.ts
+++ src/app/components/hobbies/hobbies.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Hobby {
+  title: string;
+  description: string;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-hobbies',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hobbies.component.html',
   styleUrl: './hobbies.component.css'
 })
 export class HobbiesComponent {
+  hobbies: Hobby[] = [
+    {
+      title: 'The Piano',
+      description: 'Here are some of my hobbies:',
+      imageUrl: 'assets/hobbies/piano.png'
+    },
+    {
+      title: 'E-Sport',
+      description: 'Here are some of my hobbies:',
+      imageUrl: 'assets/hobbies/piano.png'
+    }
+  ];
 
+  currentIndex = 0;
+
+  previousSlide(): void {
+    this.currentIndex = this.currentIndex === 0 ? this.hobbies.length - 1 : this.currentIndex - 1;
+  }
+
+  nextSlide(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.hobbies.length;
+  }
 }
