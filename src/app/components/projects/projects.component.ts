9ca6ab24d42fddf26c955dd6dce5f198dd1b93d2
Gabriel <gabrielgarrone670@gmail.com>
2025-05-02 20:58:54 +0200

    feat: restructure projects component with improved HTML layout and TypeScript interface

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,42 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Project {
+  title: string;
+  description: string[];
+  technologies: string[];
+  imageUrl: string;
+  link: string;
+}
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.css'
+  styleUrl: './projects.component.css',
 })
 export class ProjectsComponent {
-
+  projects: Project[] = [
+    {
+      title: 'Project 1',
+      description: [
+        'Description of project 1.',
+        'Another line of description for project 1.',
+      ],
+      technologies: ['HTML', 'CSS', 'JavaScript'],
+      imageUrl: 'assets/images/project1.jpg',
+      link: 'google.com',
+    },
+    {
+      title: 'Project 2',
+      description: [
+        'Description of project 2.',
+        'Another line of description for project 2.',
+      ],
+      technologies: ['React', 'Node.js'],
+      imageUrl: 'assets/images/project2.jpg',
+      link: 'google.com',
+    },
+  ];
 }
