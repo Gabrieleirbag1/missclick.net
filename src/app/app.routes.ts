2a7f1d1a44851d99e436e6909e38d5b4df42d0cc
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2025-05-06 09:37:18 +0200

    feat(styles): added media queries to support 16/9 feat: added admin component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },  // Default route to home
   { path: 'home', component: HomeComponent },
   { path: 'projects', component: ProjectsComponent},
+  { path: 'admin', component: AdminComponent },
   { path: '**', redirectTo: '' }  // Redirect any unknown routes to home
 ];
